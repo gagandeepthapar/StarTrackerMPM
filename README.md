2a750569acc0e16f52d681b2b6b8bab8ca4e6f63
gaganthapar12309 <gsthapar@gmail.com>
2023-01-07 19:46:43 -0800

    Updated Material and SatNode class

--- README.md
+++ README.md
@@ -28,3 +28,8 @@ In *Optical System Error Analysis and Calibration Method of High-Accuracy Star T
 
 ![SuperCentroid](media/inAction/improvedCentroidAnalysis.png)
 
+#### **Environmental Effects**
+The environment of space will create disturbances in the Star Tracker Measurement Process Model. These effects are coupled to the orbit the satellite resides within; to properly estimate how an orbit affects measurement accuracy and precision, an orbit needs to be randomly generated. The `Orbit` class represents an orbit that can be *randomized* within the distribution modeled after the orbits currently occupied by active CubeSats (and LEO satellites, in general). This was accomplished by reading in data from [CelesTrak](https://celestrak.org/) and transforming the data to a normal distribution which can be sampled (and reverse-transformed). The orbit can be plotted in matplotlib where the satellite and its body reference frame are plotted alongside the directional vector pointing to the Sun's location (based on a randomly generated Julian Date). The position of the sun and the attitude of the satellite (assumed to be nadir-pointing) directly influence the effect of temperature on the system, more specifically the focal length and optical distortion. The plot is a visualization method of the satellite's position relative to Earth and the Sun.
+
+![SampleOrbitLOS](media/inAction/Simulated_Orbit.png)
+![SampleOrbitNoLOS](media/inAction/SimulatedOrbit_noLOS.png)
--- simObjects/MaterialProperty.py
+++ simObjects/MaterialProperty.py
@@ -8,6 +8,7 @@ except:
     import constants as c
 
 from Parameter import Parameter
+from copy import deepcopy
 
 class Material:
 
@@ -15,12 +16,14 @@ class Material:
                        Emissivity:Parameter=None,
                        Area:float=None,
                        *,
-                       matName:str="Generic")->None:
+                       name:str="Generic")->None:
 
         self.abs = self.__set_parameter(Absorptivity, "Absorptivity")
         self.emi = self.__set_parameter(Emissivity, "Emissivity")
         self.area = self.__set_parameter(Area, "Area")
-        self.name = matName
+        self.name = name
+
+        self.faceVector = None
 
         return
     
@@ -35,14 +38,57 @@ class Material:
         if param is not None:
             return param
 
-        return Parameter(0.5, 0.05, 0, name)    # random parameter; will be updated to be based on known materials eg Aluminum
+        return Parameter(0.5, 0.05, 0, name=name)    # random parameter; will be updated to be based on known materials eg Aluminum
 
 class SatNode:
 
-    def __init__(self, sideA:Material, sideB:Material, sideC: Material)->None:
+    def __init__(self, faceA:Material, faceB:Material, faceC:Material, state:np.ndarray)->None:
+        
+        print(faceA is faceB)
+
+        self.faces = [faceA, deepcopy(faceA), faceB, deepcopy(faceB), faceC, deepcopy(faceC)]
+        self.state = state
+
+        # self.__set_attitude()
+
+        return
+    
+    def __repr__(self)->str:
+        return 'SAT Node'
+
+    def get_attitude(self)->tuple[np.ndarray]:
+        position = self.state[:3]
+        velocity = self.state[3:]
+
+        z = -1*position/np.linalg.norm(position)
+        
+        h = np.cross(position, velocity)
+        x = h/np.linalg.norm(h)
+
+        y = np.cross(z, x)
+        return (x, y, z)
+
+    def __set_attitude(self)->None:
+
+        faceVecs = self.get_attitude()
+        print(faceVecs)
+
+        for i in range(3):
+            print(2*i)
+            print(2*i + 1)
+            print(faceVecs[i])
+            self.faces[2*i].faceVector = faceVecs[i]
+            self.faces[2*i + 1].faceVector = -1*(faceVecs[i])
 
         return
 
-    def get_attitude(self)->tuple[np.ndarray]:
+if __name__ == '__main__':
+    al = Material(name='a')
+    bl = Material(name='b')
+    cl = Material(name='cl')
+    p = SatNode(al, al, cl, np.array([1, 2, 3, 4, 5, 6]))
 
-        return    
+    for i in range(6):
+        print(p.faces[i].faceVector)
+
+    # p.get_attitude(rv)

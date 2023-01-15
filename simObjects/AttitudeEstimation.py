17b58ca6372771d632ea94b0fc9fbfe39a67af91
gaganthapar12309 <gsthapar@gmail.com>
2023-01-14 16:51:16 -0800

    Updated QUEST algorithm to only house att. det. functions. Util functions are housed in Projection class

--- simObjects/AttitudeEstimation.py
+++ simObjects/AttitudeEstimation.py
@@ -112,117 +112,87 @@ class Projection:
         v = np.array([cvx, cvy, self.focal])
         return v/np.linalg.norm(v)
 
-    def __calc_rot_matr(self)->np.ndarray:
-        alpha = np.deg2rad(self.alpha.modulate())
-        beta = np.deg2rad(self.beta.modulate())
-        gamma = np.deg2rad(self.gamma.modulate())
+    def __random_quat(self)->np.ndarray:
+        q = np.random.uniform(0, 1, 4)
+        return q/np.linalg.norm(q)
 
-        Rz = lambda theta: np.array([[np.cos(theta), -np.sin(theta), 0],[np.sin(theta), np.cos(theta), 0],[0,0,1]])
-        Ry = lambda theta: np.array([[np.cos(theta), 0, np.sin(theta)],[0,1,0],[-np.sin(theta), 0, np.cos(theta)]])
-        Rx = lambda theta: np.array([[1,0,0],[0, np.cos(theta), -np.sin(theta)],[0, np.sin(theta), np.cos(theta)]])
-        R = Rz(gamma) @ Ry(beta) @ Rx(alpha)
+    def __quat_mult(self, x:np.ndarray)->np.ndarray:
+        e = -1*self.quat_real[:3]
+        n = self.quat_real[3]
+        
+        Ca = (2*n**2 - 1) * np.identity(3)
+        Cb = 2*np.outer(e, e)
+        Cc = -2*n*self.__skew(e)
 
-        return R
+        return (Ca + Cb + Cc)@x
+    
+    def __skew(self, n:np.ndarray)->np.ndarray:
+        return np.array([[0, -n[2], n[1]], [n[2], 0, -n[0]], [-n[1], n[0], 0]])
 
 class QUEST(Projection):
 
     def __init__(self)->None:
-        X = Parameter(0, 0.1/3, 0, name='Centroid_DEV', units='px')
-        super().__init__(X)
+        super().__init__()
         return
     
     def __repr__(self)->str:
-        name = 'QUEST SOLVER:\n{}\n'.format(self.frame)
+        name = 'QUEST SOLVER, {}:\n{}\n'.format(self.quat_real,self.frame)
         return name
 
-    def get_attitude(self)->np.ndarray:
-        B = self.__calc_B()
-        k_22 = np.trace(B)
-        K_12 = self.__calc_K12(B)
+    def get_attitude(self, *, weights:np.ndarray=None)->np.ndarray:
+        # extract eci and cv vectors
+        eci_vecs = self.frame['ECI_REAL'].to_numpy()
+        cv_vecs = self.frame['CV_EST'].to_numpy()
+        if weights is None:
+            weights = np.ones(len(eci_vecs))/len(eci_vecs)
+
+        # calc lam params
+        B = self.__calc_B(eci_vecs, cv_vecs, weights)
+        K_12, K_22 = self.__calc_K(B)
         S = B + np.transpose(B)
-
+        a, b, c, d = self.__calc_lam_params(K_12, K_22, S)
+        
         # get optimal lam
-        a = k_22**2 - np.trace(self.__calc_adjoint(S))
-        b = k_22**2 + K_12.T @ K_12
-        c = np.linalg.det(S) + K_12.T @ S @ K_12
-        d = K_12.T @ S @ S @ K_12
+        lam = self.__calc_optimal_lambda(a, b, c, d, K_22)
 
-        lam = self.__calc_optimal_lambda(a, b, c, d, k_22)
-
-        alpha = lam**2 - k_22**2 + np.trace(self.__calc_adjoint(S))
-        beta = lam - k_22
-        gamma = (lam + k_22)*alpha - np.linalg.det(S)
+        # calc quat params
+        alpha, beta, gamma = self.__calc_quat_params(lam, K_22, S)
 
         X = (alpha*np.identity(3) + beta*S + (S@S))@K_12
-
         f = np.sqrt(gamma**2 + X.T @ X)
         factor = 1/f
 
         quatE = factor * X
         quatN = factor * gamma
+        
         quat = np.array([*quatE, quatN])
         return quat/np.linalg.norm(quat)
 
-    def rot_to_quat(self, C:np.ndarray)->np.ndarray:
-
-        n = 1/2 * np.sqrt(1 + np.trace(C))
-        e1 = 1/4 * (C[1][2] - C[2][1])/n
-        e2 = 1/4 * (C[2][0] - C[0][2])/n
-        e3 = 1/4 * (C[0][1] - C[1][0])/n
-
-        q = np.array([e1, e2, e3, n])
-        return q
-
-    def quat_to_ra_dec(self, q:np.ndarray)->np.ndarray:
-        q1 = q[0]
-        q2 = q[1]
-        q3 = q[2]
-        q4 = q[3]
-
-        ra = np.arctan2(q2*q3 - q1*q4, q1*q3 + q2*q4)
-        dec = np.arcsin(-q1**2*np.sqrt(q2**2 + q3**2 + q4**2))
-        roll = np.arctan2(q2*q3 + q1*q4, -q1*q3 + q2*q4)
-
-        angs = [ra, dec, roll]
-        angDecs = [np.rad2deg(ang) for ang in angs]
-
-        return angDecs
-
-    def randomize(self, num:int=7)->None:
-        super().randomize(num)
-        self.realQuat = self.rot_to_quat(self.C.T)
-        return
-
-    def calc_diff(self, Q_real:np.ndarray, Q_calc:np.ndarray)->float:
-
-        realRA = self.quat_to_ra_dec(Q_real)
-        calcRA = self.quat_to_ra_dec(Q_calc)
-        diff = [(a-b)*3600 for (a,b) in zip(realRA, calcRA)]
-
-        return np.linalg.norm(diff)
-
-    def __calc_B(self, weights:np.ndarray=None)->np.ndarray:
-        
-        eci = self.frame['ECI'].to_numpy()
-        cv = self.frame['CV'].to_numpy()
-
-        if weights is None:
-            weights = np.ones(len(eci))/len(eci)
-
-        B = np.zeros((3,3))
-        
+    def __calc_B(self, eci:np.ndarray, cv:np.ndarray, weights:np.ndarray)->np.ndarray:
+        B = np.zeros((3,3))        
         for i in range(len(eci)):
             B += weights[i] * np.outer(eci[i], cv[i])
-            
         return np.transpose(B)
 
-    def __calc_K12(self, B:np.ndarray)->np.ndarray:
-
+    def __calc_K(self, B:np.ndarray)->tuple[np.ndarray]:
         k_12 = np.array([B[1][2] - B[2][1],
                          B[2][0] - B[0][2],
                          B[0][1] - B[1][0]])
 
-        return np.transpose(k_12)
+        return (np.transpose(k_12), np.trace(B))
+
+    def __calc_lam_params(self, K_12:np.ndarray, K_22:float, S:np.ndarray)->tuple[float]:
+        a = K_22**2 - np.trace(self.__calc_adjoint(S))
+        b = K_22**2 + K_12.T @ K_12
+        c = np.linalg.det(S) + K_12.T @ S @ K_12
+        d = K_12.T @ S @ S @ K_12
+        return (a, b, c, d)
+
+    def __calc_quat_params(self, lam:float, K_22:float, S:np.ndarray)->tuple[float]:
+        alpha = lam**2 - K_22**2 + np.trace(self.__calc_adjoint(S))
+        beta = lam - K_22
+        gamma = (lam + K_22)*alpha - np.linalg.det(S)
+        return (alpha, beta, gamma)
 
     def __calc_adjoint(self, B:np.ndarray)->np.ndarray:
         deter = np.linalg.det(B)
@@ -236,14 +206,11 @@ class QUEST(Projection):
         e1 = lam0 - self.__optimal_lambda_f(e0, a, b, c, d, k)/self.__optimal_lambda_fp(e0, a, b, c)
         err = np.abs(e1 - e0)
 
-        i = 0
+        # newton solver
         while err > eps:
-            i+=1
             e0 = e1
             e1 = e0 - self.__optimal_lambda_f(e0, a, b, c, d, k)/self.__optimal_lambda_fp(e0, a, b, c)
             err = np.abs(e1 - e0)
-
-        # print(f'iters: {i}')
         return e1
     
     def __optimal_lambda_f(self, lam:float, a:float, b:float, c:float, d:float, k:float)->float:
@@ -259,25 +226,3 @@ class QUEST(Projection):
         C = -c
         return A + B + C
 
-
-if __name__ == '__main__':
-
-    Q = QUEST()
-    
-    diff = np.empty(1_000, dtype=float)
-
-    with alive_bar(1_000) as bar:
-        for i in range(1_000):
-            Q.randomize(7)
-            q = Q.get_attitude()
-            diff[i] = Q.calc_diff(Q.realQuat, q)
-            bar()
-    
-    print(np.mean(diff))
-    print(np.std(diff))
-
-    fig = plt.figure()
-    ax= fig.add_subplot()
-    ax.hist(diff, bins=100)
-
-    plt.show()

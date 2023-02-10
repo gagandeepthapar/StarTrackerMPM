3fb713f9d1cfb8789cf3685b0da61f0705e7fa45
gaganthapar12309 <gsthapar@gmail.com>
2023-02-09 16:39:43 -0800

    added ability to plot individual params and surface plot of 2 parameters and accuracy

--- sensitivity.py
+++ sensitivity.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
 import constants as c
 from simObjects.Simulation import Simulation
@@ -25,13 +26,13 @@ class Sensitivity(Simulation):
     def __repr__(self)->str:
         return 'Sensitivity Analysis: {} Data Points'.format(self.num_runs)
     
-    def run_sim(self, param_name:str='FOCAL_LENGTH', obj_func: callable=None, *, single_sim:bool=False) -> pd.DataFrame:
+    def run_sim(self, param_names:list[str], obj_func: callable=None) -> pd.DataFrame:
         if obj_func is None:
             obj_func = self.sun_etal_hardware_analysis
 
         start = time.perf_counter()
 
-        self.__create_data(param_name)
+        self.__create_data(param_names)
         self.sim_data['CALC_ACCURACY'] = self.sim_data.apply(obj_func, axis=1)
 
         end = time.perf_counter()
@@ -39,74 +40,69 @@ class Sensitivity(Simulation):
 
         return
 
-    def plot_data(self, plot_params:bool=False) -> None:
+    def plot_data(self, params:list[str]) -> None:
+        if len(params) < 2:
+            params.append('FOCAL_LENGTH') 
         
-        fig = plt.figure()
-        ax = fig.add_subplot()
+        if len(params) < 1:
+            params.append('FOCAL_ARRAY_INCLINATION')    
+            
 
-        ax.hist(self.sim_data['CALC_ACCURACY'], bins=int(np.sqrt(self.num_runs)))
-        ax.set_ylabel('Number of Runs')
-        ax.set_xlabel('Calculated Accuracy [arcsec]')
-        ax.set_title('Star Tracker Accuracy: {} +/-{} arcsec:\n{:,} Runs'.\
-                    format(np.round(self.sim_data['CALC_ACCURACY'].mean(),3),\
-                           np.round(self.sim_data['CALC_ACCURACY'].std(),3),
-                           self.num_runs))
-
-        if plot_params:
-            param_fig = plt.figure()
-            size = (3, 2)
-            params = ['FOCAL_LENGTH', 'FOCAL_ARRAY_INCLINATION', 'DISTORTION', 'PRINCIPAL_POINT_ACCURACY', 'BASE_DEV_X', 'TEMP']
-
-            for i, param in enumerate(params):
-                param_ax = param_fig.add_subplot(size[0], size[1], i+1)
-
-                param_ax.hist(self.sim_data[param], bins=int(np.sqrt(self.num_runs)), label=param)
-                param_ax.set_title('{}: {} +/- {}'.\
-                                   format(param.replace('_', ' ').title(),\
-                                          np.round(self.sim_data[param].mean(), 3),\
-                                          np.round(self.sim_data[param].std(), 3)))
-
-                if param == 'FOCAL_LENGTH':
-                    param_ax.axvline(self.camera.f_len.ideal, color='r', label='True Focal Length ({} px)'.format(np.round(self.camera.f_len.ideal,3)))
-                    param_ax.legend()
-                    
-                
-                if param == 'BASE_DEV_X':
-                    param_ax.hist(self.sim_data['BASE_DEV_Y'], color='g', label='BASE_DEV_Y', bins=int(np.sqrt(self.num_runs)))
-                    param_ax.legend()
-                    param_ax.set_title('Centroiding Accuracy:\n{} +/- {} (X); {} +/- {} (Y)'.\
-                                       format(np.round(self.sim_data['BASE_DEV_X'].mean(),3),\
-                                              np.round(self.sim_data['BASE_DEV_X'].std(),3),\
-                                              np.round(self.sim_data['BASE_DEV_Y'].mean(),3),\
-                                              np.round(self.sim_data['BASE_DEV_Y'].std(),3)))
-                    
-        return
+        self.__plot_surface_data(params[0], params[1])
+        super().plot_data()
+        return 
     
-    def __create_data(self, param_name:str)->pd.DataFrame:
+    def __plot_surface_data(self, paramA:str, paramB:str)->None:
+
+        row = deepcopy(self.sim_data.iloc[0].squeeze())
+
+        paramA = self.params[paramA]
+        paramB = self.params[paramB]
+
+        param_a_range = np.linspace(paramA.minRange, paramA.maxRange, int(np.sqrt(self.num_runs)))
+        param_b_range = np.linspace(paramB.minRange, paramB.maxRange, int(np.sqrt(self.num_runs)))
+
+        p_a_mesh, p_b_mesh = np.meshgrid(param_a_range, param_b_range)
+        acc = np.zeros(np.shape(p_a_mesh))
+
+        for i in range(len(p_a_mesh)):
+            for j in range(len(p_a_mesh[0])):
+                row[paramA.name] = p_a_mesh[i][j]
+                row[paramB.name] = p_b_mesh[i][j]
+                
+                acc[i][j] = self.sun_etal_hardware_analysis(row)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+
+        ax.plot_surface(p_a_mesh, p_b_mesh, acc, cmap='coolwarm')
+
+        ax.set_xlabel(paramA.name)
+        ax.set_ylabel(paramB.name)
+        ax.set_zlabel('Calculated Accuracy [arcsec]')
+
+        ax.set_title('Surface Map of Accuracy')
+
+        return
+
+    def __create_data(self, param_names:list[str])->pd.DataFrame:
+
+        if type(param_names) is not list:
+            param_names = [param_names]
 
         # idealize parameters from components
         f_data = self.camera.ideal(num=self.num_runs)
         c_data = self.centroid.ideal(num=self.num_runs)
         o_data = self.orbit.ideal(num=self.num_runs)
 
-        # modulate parameter
-        if param_name in f_data.columns:
-            f_data[param_name] = self.camera.params[param_name].modulate(self.num_runs)
-
-            if param_name == 'FOCAL_LENGTH':
-                f_data['D_FOCAL_LENGTH'] = f_data['FOCAL_LENGTH'] - self.camera.f_len.ideal
-        
-        if param_name == 'CENTROID':
-            c_data['BASE_DEV_X'] = self.centroid.centroid.modulate(self.num_runs)
-            c_data['BASE_DEV_Y'] = self.centroid.centroid.modulate(self.num_runs)
-        
-        if param_name == 'IDENTIFICATION':
-            pass
-        
-        if param_name in o_data.columns:
-            o_data[param_name] = self.orbit.params[param_name].modulate(self.num_runs)
-        
         self.sim_data = pd.concat([f_data, c_data,o_data], axis=1)
+
+        # modulate all indicated params in argument
+        for param in param_names:
+            self.sim_data[param] = self.params[param].modulate(self.num_runs)
+            
+            if param == 'FOCAL_LENGTH': # update d_focal_length
+                self.sim_data['D_FOCAL_LENGTH'] = self.sim_data['FOCAL_LENGTH'] - self.camera.f_len.ideal
 
         logger.info('params:\n{}\n{}'.format(self.sim_data.mean(), self.sim_data.std()))
 

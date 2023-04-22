9243e2866028acb8fe193ac231ab520ed45b24f4
gaganthapar12309 <gsthapar@gmail.com>
2023-04-21 19:44:59 -0700

    removing identification from software class

--- simObjects/Software.py
+++ simObjects/Software.py
@@ -21,19 +21,19 @@ class Software:
                        id_json:str=c.IDEAL_IDENT):
 
         self.dev_x, self.dev_y = self. __set_centroid(centroiding, ctr_json)
-        self.identification = self.__set_ident(identification, id_json)
+        # self.identification = self.__set_ident(identification, id_json)
 
-        self.fail_ident = self.__set_ident(identification, id_json, name='FAIL_IDENT_RATE')
+        # self.fail_ident = self.__set_ident(identification, id_json, name='FAIL_IDENT_RATE')
         self.data = self.randomize()
         
-        self.__param_list = [self.dev_x, self.dev_y, self.fail_ident]
+        self.__param_list = [self.dev_x, self.dev_y]#, self.fail_ident]
         # self.identification, 
         self.params = {param.name: param for param in self.__param_list}
 
         return
 
     def __repr__(self)->str:
-        return 'Centroid: {}\nIdentification:{}'.format(self.dev_x, self.identification)
+        return 'Centroid: {}'.format(self.dev_x)
 
     def randomize(self, num:int=1)->pd.DataFrame:
     
@@ -41,7 +41,7 @@ class Software:
         df['BASE_DEV_X'] = self.dev_x.modulate(num)
         df['BASE_DEV_Y'] = self.dev_y.modulate(num)
         # df['IDENTIFICATION_ACCURACY'] = self.identification.modulate(num)
-        df['FAIL_IDENT_RATE'] = self.fail_ident.modulate(num)
+        # df['FAIL_IDENT_RATE'] = self.fail_ident.modulate(num)
         # df['FALSE_IDENT_RATE'] = self.false_ident.modulate(num)
 
         self.data = df
@@ -68,19 +68,22 @@ class Software:
         stddev = ctr_data['STDDEV']
         units = ctr_data['UNITS']
 
-        devX = Parameter(ideal, stddev, mean, name='BASE_DEV_X', units=units)
-        devY = Parameter(ideal, stddev, mean, name='BASE_DEV_Y', units=units)
+        # devX = Parameter(ideal, stddev, mean, name='BASE_DEV_X', units=units)
+        # devY = Parameter(ideal, stddev, mean, name='BASE_DEV_Y', units=units)
+
+        devX = Parameter(0, stddev, name='BASE_DEV_X')
+        devY = Parameter(0, stddev, name='BASE_DEV_Y')
 
         return devX, devY
     
-    def __set_ident(self, param:Parameter, json_path:str, name:str='IDENTIFICATION_ACCURACY')->UniformParameter:
-        if param is not None:
-            return param
+    # def __set_ident(self, param:Parameter, json_path:str, name:str='IDENTIFICATION_ACCURACY')->UniformParameter:
+    #     if param is not None:
+    #         return param
         
-        with open(json_path) as fp_open:
-            id_data = jsonload(fp_open)
+    #     with open(json_path) as fp_open:
+    #         id_data = jsonload(fp_open)
 
-        min_acc = 0.9#id_data['MIN_ACCURACY']
-        ident = UniformParameter(min_acc, 1.0, name=name, units='Percent')
+    #     min_acc = 0.9#id_data['MIN_ACCURACY']
+    #     ident = UniformParameter(min_acc, 1.0, name=name, units='Percent')
 
-        return ident
+    #     return ident
